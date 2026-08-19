433e42e63d973d385365a825cadab561ff9f8047
SOKCHHORN PC <sokchhorn.pc@users.noreply.github.com>
2026-08-19 08:16:13 +0700

    Update paths for new location C:\deep-video-downloader (moved out of OneDrive)

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d "C:\Users\SOKCHHORN PC\OneDrive\Desktop\Project WorkSpace\deep-video-downloader"
+cd /d "C:\deep-video-downloader"
 node_modules\electron\dist\electron.exe --no-sandbox --disable-gpu --disable-dev-shm-usage .
